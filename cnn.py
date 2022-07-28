bc8752b937236fd019eeac42cceca9cebcc61f0c
macieksww <macieksw2000@gmail.com>
2022-07-28 16:31:46 +0200

    updates

--- cnn.py
+++ cnn.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import numpy as np
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from import_cv import process_directory
@@ -16,7 +17,7 @@ from keras import constraints
 from keras import losses 
 from keras import optimizers 
 from keras import metrics 
-from keras.callbacks import CSVLogger
+from keras.callbacks import CSVLogger, EarlyStopping
 from save_train_data import data_saver
 
 def prep_dataset():
@@ -28,9 +29,10 @@ def prep_dataset():
     print(num_of_images)
     # number of samples used in every training episode
     # batch_size = int(num_of_images/15)
-    epochs = 50
+    epochs = 1
     batch_size = 32
     load_model = False
+    early_stopping = False
     print("BATCH SIZE")
     print(batch_size)
     image_size = (300, 432)
@@ -54,6 +56,20 @@ def prep_dataset():
         image_size=(image_size[0], image_size[1]),
         batch_size=batch_size)
     
+    # preparation of filenames to later set predictions with validation files
+    print("FILENAMES")
+    file_paths = val_ds.file_paths
+    temp_file_paths = []
+    for file_path in file_paths:
+        if "non" in file_path:
+            file_path = file_path[89:]
+        else:
+            file_path = file_path[86:]
+        file_path = np.array([file_path])
+        temp_file_paths.append(file_path)
+    file_paths = np.array(temp_file_paths)
+
+
     class_names = train_ds.class_names
     print("DATA CLASSES:")
     print(class_names)
@@ -91,7 +107,19 @@ def prep_dataset():
 
     csv_logger = CSVLogger("/home/bdroix/bdroix/aesthetics_analysis/model_history_log_adagrad_32.csv", append=False)
     model.summary()
-    model.fit(train_ds, batch_size = batch_size, validation_data = val_ds, epochs=epochs, callbacks=[csv_logger])
+
+    # stopping after reaching certain accuracy instead of constant num of epochs
+    es = EarlyStopping(monitor='val_accuracy', mode='min', verbose=1)
+
+    # if early_stopping is False:
+    history = model.fit(train_ds, batch_size = batch_size, validation_data = val_ds, epochs=epochs, callbacks=[csv_logger])
+    # else:
+    #     history = model.fit(train_ds, batch_size = batch_size, validation_data = val_ds, epochs=epochs, callbacks=[csv_logger, es])
+    
+    predictions = model.predict(val_ds, verbose=0)
+    print("PREDICTIONS")
+    predictions = np.concatenate((file_paths, predictions), axis=1)
+    print(predictions)
 
     loss, accuracy = model.evaluate(val_ds, verbose=1) 
     loss = "{:.4f}".format(loss)
